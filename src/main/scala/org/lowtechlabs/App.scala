f317a348eeaa81caed52d3e785f9f896da563332
Morgan Todd <tx@lowtech-labs.org>
2010-05-31 18:34:44 -0700

    moved synch villein to a seperate method

--- src/main/scala/org/lowtechlabs/App.scala
+++ src/main/scala/org/lowtechlabs/App.scala
@@ -4,7 +4,7 @@ package org.lowtechlabs
 
 import org.linkedprocess.villein._
 import org.linkedprocess.LinkedProcess._
-import patterns.SynchronousPattern
+import patterns.{TimeoutException, SynchronousPattern}
 import proxies.{FarmProxy, JobProxy, VmProxy}
 
 /**
@@ -24,28 +24,61 @@ object App {
   val password = "secret"
 
   def main(args: Array[String]) {
-    val villein = new Villein(server, port, username, password);
+    val villein = new Villein(server, port, username, password)
     villein.createCloudFromRoster
-    for(i <- 0 until 3600){
-      val farmProxies = villein.getCloudProxy.getFarmProxies.toArray.toList
+    for(i <- 0 until 60){
+      val farmProxies: List[FarmProxy] = villein.getCloudProxy.getFarmProxies.toArray.toList.asInstanceOf[List[FarmProxy]]
       if(farmProxies.length > 0) {
-        val farmResult = SynchronousPattern.spawnVm(farmProxies.head.asInstanceOf[FarmProxy], "groovy", 10000)
-        if (farmResult.wasSuccessful) {
-          val vmProxy = farmResult.getSuccess
-          val jobProxy = new JobProxy
-          jobProxy.setExpression("1 + 2;")
-          val jobResult = SynchronousPattern.submitJob(vmProxy, jobProxy, 5000)
-          if (jobResult.wasSuccessful) println(jobResult.getSuccess.getResult)
-          else {println(jobResult.getLopError()); exit()}
-          SynchronousPattern.terminateVm(vmProxy, -1)
-        } else {
-          println(farmResult.getLopError())
-          exit()
+        try {
+          spawnSynchronousVillein(farmProxies.head)
+        } catch {
+            case e: TimeoutException => {
+              println("It took longer than 5 seconds to execute this job.")
+              exit(1)
+            }
         }
       } else {
         println("Try #" +i+ "; no farms found!")
       }
       Thread.sleep(1L)
+    }
+  }
+  /*
+    // Synchronous Villein Java implementation
+    JobProxy jobProxy = new JobProxy();
+    jobProxy.setExpression("1 + 2;");
+    try {
+      ResultHolder<JobProxy> result = SynchronousPattern.submitJob(vmProxy, jobProxy, 5000);
+      if(result.wasSuccessful()) {
+        System.out.println(result.getSuccess().getResult());
+      } else {
+        System.out.println(result.getLopError());
+        System.exit(1);
+      }
+    } catch(TimeoutException e) {
+      System.out.println("It took longer than 5 seconds to execute this job.");
+      System.exit(1);
+    }
+
+   */
+  def spawnSynchronousVillein(farmProxy: FarmProxy) = {
+    val startTime = System.currentTimeMillis
+    val farmResult = SynchronousPattern.spawnVm(farmProxy, "groovy", 10000)
+    if (farmResult.wasSuccessful) {
+      val vmProxy = farmResult.getSuccess
+      val jobProxy = new JobProxy
+      jobProxy.setExpression("1 + 2;")
+      val jobResult = SynchronousPattern.submitJob(vmProxy, jobProxy, 5000)
+      if (jobResult.wasSuccessful) {
+        println(jobResult.getSuccess.getResult)
+        SynchronousPattern.terminateVm(vmProxy, (System.currentTimeMillis - startTime))
+      } else {
+        println(jobResult.getLopError())
+        exit()
+      }
+    } else {
+      println(farmResult.getLopError())
+      exit()
     }
   }
 }
